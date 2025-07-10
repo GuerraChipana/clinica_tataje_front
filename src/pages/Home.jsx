03fc2abbc55efb9054781f9dc98d3a11a3bfb4f6
tracy <tracygonzales694@gmail.com>
2025-07-10 11:39:28 -0500

    barra lateral menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
+import '../styles/admin/OffcanvasCustom.css';
 import '../styles/admin/CarouselCustom.css';
 
 function Home() {
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">Clínica</Link>
+
+          {/* Botón hamburguesa solo visible en pantallas pequeñas */}
           <button
-            className="navbar-toggler"
+            className="navbar-toggler d-lg-none"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#offcanvasNavbar"
@@ -17,8 +20,22 @@ function Home() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+
+          {/* Menú visible solo en pantallas grandes */}
+          <div className="collapse navbar-collapse d-none d-lg-flex justify-content-end">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/sobre-nosotros">Sobre Nosotros</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/paciente">Paciente</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/historial">Historial</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/contacto">Contacto</Link></li>
+            </ul>
+            <Link to="/login-paciente" className="btn btn-outline-light ms-3">Iniciar Sesión</Link>
+          </div>
+
+          {/* Offcanvas para pantallas pequeñas */}
           <div
-            className="offcanvas offcanvas-end text-bg-dark"
+            className="offcanvas offcanvas-end text-bg-dark d-lg-none"
             tabIndex="-1"
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
@@ -32,25 +49,23 @@ function Home() {
                 aria-label="Cerrar"
               ></button>
             </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <div className="offcanvas-body d-flex flex-column justify-content-between">
+              <ul className="navbar-nav">
                 <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/sobre-nosotros">Sobre Nosotros</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/paciente">Paciente</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/historial">Historial</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/contacto">Contacto</Link></li>
-                <li className="nav-item mt-3"><Link to="/login-paciente" className="btn btn-outline-light w-100">Iniciar Sesión</Link></li>
               </ul>
+              <Link to="/login-paciente" className="btn btn-outline-light mt-3 w-100">Iniciar Sesión</Link>
             </div>
           </div>
         </div>
       </nav>
 
-      <div style={{ marginTop: '56px' }}></div>
-
-      <Carousel>
+      <Carousel controls={false} indicators={true}>
         <Carousel.Item>
-          <img className="d-block w-100" src="/images/banner1.jpg" alt="Primer banner" />
+          <img className="d-block w-100" src="/images/banner1.jpg" alt="Banner 1" style={{ height: '350px', objectFit: 'cover' }} />
           <Carousel.Caption>
             <h3>Miércoles de Bienestar</h3>
             <p>Atiende tu salud con los mejores especialistas</p>
@@ -58,7 +73,7 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src="/images/banner2.jpg" alt="Segundo banner" />
+          <img className="d-block w-100" src="/images/banner2.jpg" alt="Banner 2" style={{ height: '350px', objectFit: 'cover' }} />
           <Carousel.Caption>
             <h3>Cuidamos de ti</h3>
             <p>Reserva tus citas fácilmente</p>
